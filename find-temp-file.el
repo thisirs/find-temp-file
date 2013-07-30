00c21c0feafaa9c05ccc6e8f40a5080be5d4862a
thisirs <thisirs@gmail.com>
2013-07-30 11:06:14 +0200

    Fix missing paren

--- find-temp-file.el
+++ find-temp-file.el
@@ -90,7 +90,7 @@ contains a dot, use EXTENSION as the full file name."
    (let ((file-path (find-temp-file--filename extension)))
      (make-directory (file-name-directory file-path) :parents)
      file-path)
-  (basic-save-buffer))
+  (basic-save-buffer)))
 
 (defun find-temp-file--filename (&optional extension-or-file)
   "Return a full path of a temporary file to be opened. If
